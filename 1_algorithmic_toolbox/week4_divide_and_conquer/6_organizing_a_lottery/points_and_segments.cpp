634d8381031490de57325805f1f4b4683340bc39
Blargian <shaunstruwig.ss@gmail.com>
2023-12-25 20:01:52 +0100

    lottery problem with edge case failing

--- 1_algorithmic_toolbox/week4_divide_and_conquer/6_organizing_a_lottery/points_and_segments.cpp
+++ 1_algorithmic_toolbox/week4_divide_and_conquer/6_organizing_a_lottery/points_and_segments.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
+#include <algorithm>
+#include <utility>
 
 using std::vector;
-
-vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
-  vector<int> cnt(points.size());
-  //write your code here
-  return cnt;
-}
 
 vector<int> naive_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
   vector<int> cnt(points.size());
@@ -18,6 +15,37 @@ vector<int> naive_count_segments(vector<int> starts, vector<int> ends, vector<in
   }
   return cnt;
 }
+
+vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
+  
+  vector<int> cnt(points.size(),0);
+  vector<std::pair<int,int>> segments; 
+
+  for(auto i = starts.begin(); i < starts.end(); i++) {
+    auto index = std::distance(starts.begin(),i);
+    segments.push_back(std::make_pair(starts[index],ends[index]));  
+  };
+
+  std::sort(segments.begin(), segments.end(), [](std::pair<int,int> a, std::pair<int,int> b){ return a.first < b.first;});
+  std::sort(points.begin(), points.end());
+
+  auto p_it = points.begin();
+  for (p_it; p_it < points.end(); ++p_it) {
+    int count = 0;
+    auto seg_it = segments.begin();
+    for (seg_it ; seg_it < segments.end() ; ++seg_it){
+      if (seg_it->first <= *p_it && *p_it <= seg_it->second ){
+        count++;
+	cnt[std::distance(p_it,points.begin())] = count;
+      } else {      
+        segments.erase(segments.begin(),seg_it-1);
+        break;
+      }
+    }
+  }
+  return cnt;
+}
+
 
 int main() {
   int n, m;
@@ -31,7 +59,7 @@ int main() {
     std::cin >> points[i];
   }
   //use fast_count_segments
-  vector<int> cnt = naive_count_segments(starts, ends, points);
+  vector<int> cnt = fast_count_segments(starts, ends, points);
   for (size_t i = 0; i < cnt.size(); i++) {
     std::cout << cnt[i] << ' ';
   }
